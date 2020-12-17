76e27b770c8b582a2b8bc5a0bf05bb8ffe0db311
Brian Ketelsen <mail@bjk.fyi>
2020-12-17 12:01:10 -0500

    more metadata

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,8 +33,8 @@ export default function Hero() {
         {SITE_DESCRIPTION}
       </h2>
       <h1 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
-        The first {BRAND_NAME}
-        <br className={styleUtils['show-on-desktop']} /> global user conference
+        The largest {BRAND_NAME}
+        <br className={styleUtils['show-on-desktop']} /> conference in the world.
       </h1>
       <h2
         className={cn(
@@ -50,7 +50,7 @@ export default function Hero() {
         <p>{DATE}</p>
         <div className={styles['description-separator']} />
         <p>
-          <strong>Online</strong>
+          <strong>Online | San Diego, California</strong>
         </p>
       </div>
     </div>
--- lib/constants.ts
+++ lib/constants.ts
@@ -17,13 +17,13 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'gophercon';
-export const BRAND_NAME = 'GopherCon';
+export const BRAND_NAME = 'Go';
 export const SITE_NAME_MULTILINE = ['GopherCon', 'Conf'];
 export const SITE_NAME = 'GopherCon';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'The largest conference in the world for Go developers.';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
+  'An interactive hybrid online and in-person experience by the community for everyone.';
 export const DATE = 'October 27, 2020';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
 export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
