6bc6933b395e2f45e26c868df68c66a3eb162a54
Brian Ketelsen <mail@bjk.fyi>
2020-12-18 09:59:56 -0500

    read scope instead of full

--- pages/api/auth/[[...nextauth]].js
+++ pages/api/auth/[[...nextauth]].js
@@ -14,6 +14,7 @@ const options = {
 
     // A database is optional, but required to persist accounts in a database
     // database: process.env.DATABASE_URL,
+    secret: process.env.NEXT_SESSION_SECRET,
 }
 
 export default (req, res) => NextAuth(req, res, options)
