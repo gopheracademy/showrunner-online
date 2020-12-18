487733a2a7ccdfc7780e7fde7e9cb94f85b41f46
Brian Ketelsen <mail@bjk.fyi>
2020-12-18 09:03:37 -0500

    read scope instead of full

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Conf() {
       </>}
       {session && <>
         Signed in as {session.user.name} <br />
-        console.log(session.accessToken);
+        {        console.log(session.accessToken)}
         <button onClick={() => { signOut() }}>Sign out</button>
       </>}
       <ConfContent
