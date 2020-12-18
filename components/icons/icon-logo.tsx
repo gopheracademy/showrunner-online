56978db18dd5944fc9149764aaf36fcd2f035629
okryuk <akryuk@osdacorp.com>
2020-12-18 13:08:09 -0500

    Changed header's logo to Gophercon

--- components/icons/icon-logo.tsx
+++ components/icons/icon-logo.tsx
@@ -20,21 +20,11 @@ export default function IconLogo({
   ...props
 }) {
   return (
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <rect width="100%" height="100%" rx="16" fill={backgroundColor} />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill={foregroundColor}
-      />
-    </svg>
+    <img name="logo-url"
+         width="180%"
+         height="180%"
+         src="https://res.cloudinary.com/bizzaboprod/image/upload/c_crop,g_custom,f_auto/v1596657274/pern6ovrtyyg9wctavxz.png"
+         alt="GopehrCon logo image"
+     />
   );
 }
